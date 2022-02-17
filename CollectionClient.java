5ffe37acba8da7997706f2974120f3ccf61d8e64
Sarah Nowotny <snowotny47@gmail.com>
2022-02-17 09:28:29 -0600

    started working on part 2

--- CollectionClient.java
+++ CollectionClient.java
@@ -1,29 +1,29 @@
 import java.util.List;
-import java.util.ArrayList;
-import java.util.List;
+//import java.util.ArrayList;
+//import java.util.List;
 
 public class CollectionClient {
     public static void main(String[] args) throws Exception {
-     /*   ArrayList<Collectable> collection = new ArrayList<Collectable>();
-        collection.add("Hazmat");
-        collection.add("RoboHodags");
-        collection.add("Supposable Thumbs");
-        collection.add("Patronum Bots");
-        collection.add("Viking Robotics");
-        collection.add("Men in Kilts");
-        collection.add("Great Scott");
-        collection.add("Kracken Pinion");
-        collection.add("Panicking Coconuts");
-        collection.add("The Knack");
-        collection.add("Incredible Thumbs");
-        collection.add("TBD");
-        collection.add("LaserTech");
-        collection.add("RoboDons");
-        collection.add("Techno G.O.A.T.S.");
-        collection.add("Crimson Cruasders");
-        collection.add("Chair Chasers");
-        collection.add("Hortonville Robotics");
-        collection.add("Kinetic Polar Bears");
+        /*ArrayList<Collectable> collection = new ArrayList<Collectable>();
+        collection.add(0,"Hazmat");
+        collection.add(1,"RoboHodags");
+        collection.add(2,"Supposable Thumbs");
+        collection.add(3,"Patronum Bots");
+        collection.add(4,"Viking Robotics");
+        collection.add(5,"Men in Kilts");
+        collection.add(6,"Great Scott");
+        collection.add(7,"Kracken Pinion");
+        collection.add(8,"Panicking Coconuts");
+        collection.add(9,"The Knack");
+        collection.add(10,"Incredible Thumbs");
+        collection.add(11,"TBD");
+        collection.add(12,"LaserTech");
+        collection.add(13,"RoboDons");
+        collection.add(14,"Techno G.O.A.T.S.");
+        collection.add(15,"Crimson Cruasders");
+        collection.add(16,"Chair Chasers");
+        collection.add(17,"Hortonville Robotics");
+        collection.add(18,"Kinetic Polar Bears");
         System.out.println(collection);
         //Collectable collection.setSize(19);
 */
